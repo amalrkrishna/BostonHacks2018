57f9331e58ad213fa293067f3883ccd294f7199d
Guanhua Lee <leebond_guanhua@hotmail.com>
2019-04-03 17:35:20 -0400

    update

--- parkngo/app.js
+++ parkngo/app.js
@@ -10,6 +10,7 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
 var leaserSchema = new mongoose.Schema({
 	name: String,
 	title: String,
